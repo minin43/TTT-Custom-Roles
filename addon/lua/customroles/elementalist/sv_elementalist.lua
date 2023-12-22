da4e44892ec632c39c71373473866a401f70dc6e
Logan <lobsterlogan43@yahoo.com>
2023-12-22 17:08:10 -0600

    Squashed commit of the following:
    commit 0d25b1d061a9c8760075ca829030f485df428c38
    Author: Logan Christianson <lobsterlogan43@yahoo.com>
    Date:   Fri Dec 22 12:23:21 2023 -0600
    
        a

--- addon/lua/customroles/elementalist/sv_elementalist.lua
+++ addon/lua/customroles/elementalist/sv_elementalist.lua
@@ -1,12 +1,15 @@
 util.AddNetworkString("BeginIceScreen")
 util.AddNetworkString("EndIceScreen")
+util.AddNetworkString("BeginDimScreen")
+util.AddNetworkString("EndDimScreen")
 
 local function IsValidPlayerEnt(ent)
     return ent and IsValid(ent) and ent:IsPlayer() and ent:Alive()
 end
 
 local ChilledPlayers = {}
-local LitPlayers = {}
+local IgnitedPlayers = {}
+local BlindedPlayers = {}
 
 hook.Add("TTTSpeedMultiplier", "Frostbite Effect", function(ply, mults)
     local effect = ChilledPlayers[ply:SteamID64()]
@@ -19,7 +22,7 @@ end)
 hook.Add("EntityTakeDamage", "Elementalist Effects", function(ent, dmginfo)
     local att = dmginfo:GetAttacker()
 
-    if not IsValidPlayerEnt(ent) or not IsValidPlayerEnt(att) or not dmginfi:IsBulletDamage() then
+    if not IsValidPlayerEnt(ent) or not IsValidPlayerEnt(att) or not dmginfo:IsBulletDamage() then
         return end
     end
 
@@ -36,8 +39,10 @@ hook.Add("EntityTakeDamage", "Elementalist Effects", function(ent, dmginfo)
     if att:HasEquipmentItem(EQUIP_ELEMENTALIST_FROSTBITE) then
         local MovementSlow = math.Round(math.Clamp(20 + (scale * 20), 20, 40))
         local Timer = math.Round(math.Clamp(1 + scale, 1, 2), 1)
+        local chance = math.random(20) == 1
 
-        if att:HasEquipmentItem(EQUIP_ELEMENTALIST_FROSTBITE_UP) and ChilledPlayers[vicId] then
+        if att:HasEquipmentItem(EQUIP_ELEMENTALIST_FROSTBITE_UP) and ChilledPlayers[vicId] and chance then
+            --Upgrade functionality
             ChilledPlayers[vicId] = false
             ent:Freeze(true)
 
@@ -45,6 +50,7 @@ hook.Add("EntityTakeDamage", "Elementalist Effects", function(ent, dmginfo)
                 net.WriteBool(true)
             net.Send(ent)
         else
+            --Base fucntionality
             ChilledPlayers[vicId] = 1 - (MovementSlow * 0.01)
             
             net.Start("BeginIceScreen")
@@ -60,29 +66,68 @@ hook.Add("EntityTakeDamage", "Elementalist Effects", function(ent, dmginfo)
             net.Start(ent)
         end
 
-        if not timer.Exists("frostbite_" .. vicId) then
-            timer.Create("frostbite_" .. vicId, Timer, 1, EndFrostbite)
+        if not timer.Exists(vicId .. "_IsSlowed") then
+            timer.Create(vicId .. "_IsSlowed", Timer, 1, EndFrostbite)
         else
-            timer.Adjust("frostbite_" .. vicId, Timer, 1, EndFrostbite)
+            timer.Adjust(vicId .. "_IsSlowed", Timer, 1, EndFrostbite)
         end
     end
 
     if att:HasEquipmentItem(EQUIP_ELEMENTALIST_PYROMANCER) then
-        if att:HasEquipmentItem(EQUIP_ELEMENTALIST_PYROMANCER_UP) then
+        local chance = math.random(20) == 1
+
+        if att:HasEquipmentItem(EQUIP_ELEMENTALIST_PYROMANCER_UP) and IgnitedPlayers[vicId] and chance then
             --Upgrade functionality
+            local timerId = vicId .. "_IsBurningShotgunFix"
+            if timer.Exists() then return end
+            timer.Create(timerId, 0.1, 1, function()
+                timer.Remove(timerId)
+            end)
+
+            local explosion = ents.Create( "env_explosion" )
+
+            if IsValid(explosion) then
+                IgnitedPlayers[vicId] = nil
+                ent:Extinguish()
+
+                explosion:SetPos(ent:GetPos())
+                explosion:SetOwner(att)
+                explosion:Spawn()
+                explosion:SetKeyValue("iMagnitude", ent:Health() * 2)
+                explosion:Fire("Explode", 0, 0)
+                util.BlastDamage(explosion, att, ent:GetPos(), ent:Health() * 2, ent:Health()) 
+            end
         else
             --Base functionality
-            LitPlayers[vicId] = true
+            IgnitedPlayers[vicId] = true
+            local timerID = vicId .. "_IsBurning"
 
-            local timeToBurn = 1 + (3 * scale)
+            local timeToBurn = 1 + (4 * scale)
+
+            if not timer.Exists(timerId) or timeToBurn > timer.TimeLeft(timerId)
+            ent:Ignite(timeToBurn, 400 * scale)
+
+            local function removeBurningStatus(ply)
+                IgnitedPlayers[ply:SteamID64()] = false
+                ply:Extinguish()
+            end
+
+            if timer.Exists(timerId) then
+                timer.Adjust(timerId, timeToBurn, 1, removeBurningStatus(ent))
+            else
+                timer.Create(timerId, timeToBurn, 1, removeBurningStatus(ent))
+            end
         end
     end
 
     if att:HasEquipmentItem(EQUIP_ELEMENTALIST_WINDBURN) then
-        if att:HasEquipmentItem(EQUIP_ELEMENTALIST_WINDBURN_UP) then
+        local chance = math.random(20) == 1
+
+        if att:HasEquipmentItem(EQUIP_ELEMENTALIST_WINDBURN_UP) and chance then
             --Upgrade functionality
         else
             --Base functionality
+            dmginfo:SetDamageForce(1000 * scale) -- This really all this needs to be?
         end
     end
 
@@ -91,14 +136,52 @@ hook.Add("EntityTakeDamage", "Elementalist Effects", function(ent, dmginfo)
             --Upgrade functionality
         else
             --Base functionality
+            local edata = EffectData()
+            if edata then -- Does this need to be sent to clients? It's ran in shared in weapon_ttt_stungun, where this is pulled from...
+                edata:SetEntity(ent)
+                edata:SetMagnitude(3)
+                edata:SetScale(2)
+
+                util.Effect("TeslaHitBoxes", edata)
+            end
+
+            local eyeang = ent:EyeAngles()
+            if eyeang then
+                local j = 10
+                eyeang.pitch = math.Clamp(eyeang.pitch + math.Rand(-j, j), -90, 90)
+                eyeang.yaw = math.Clamp(eyeang.yaw + math.Rand(-j, j), -90, 90)
+                ent:SetEyeAngles(eyeang)
+            end
         end
     end
 
     if att:HasEquipmentItem(EQUIP_ELEMENTALIST_MIDNIGHT) then
-        if att:HasEquipmentItem(EQUIP_ELEMENTALIST_MIDNIGHT_UP) then
+        local chance = math.random(20) == 1
+        
+        if att:HasEquipmentItem(EQUIP_ELEMENTALIST_MIDNIGHT_UP) and BlindedPlayers[vicId] and chance then
             --Upgrade functionality
+            BlindedPlayers[vicId] = 100
         else
             --Base functionality
+            BlindedPlayers[vicId] = BlindedPlayers[vicId] or 0
+            BlindedPlayers[vicId] = math.Clamp(BlindedPlayers[vicId] + (40 * scale), 0, 50)            
+        end
+
+        net.Start("BeginDimScreen")
+            net.WriteInt(BlindedPlayers[vidId])
+        net.Send(ent)
+
+        local function EndBlind()
+            BlindedPlayers[vicId] = 0
+
+            net.Start("EndDimScreen")
+            net.Send(ent)
+        end
+
+        if not timer.Exists(vicId .. "_IsBlind") then
+            timer.Create(vicId .. "_IsBlind", 2, 1, EndBlind)
+        else
+            timer.Adjust(vicId .. "_IsBlind", 2, 1, EndBlind)
         end
     end
 
@@ -107,6 +190,12 @@ hook.Add("EntityTakeDamage", "Elementalist Effects", function(ent, dmginfo)
             --Upgrade functionality
         else
             --Base functionality
+            local dmg = dmginfo:GetDamage()
+            local healAmount = math.Round(dmg * 0.33, 0)
+
+            att:SetHealth(math.Clamp(att:Health() + healAmount), 0, att:GetMaxHealth())
         end
     end
 end)
+
+-- Should I include a check to end all effects on a player death?
