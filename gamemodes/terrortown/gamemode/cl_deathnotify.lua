f17c4131484433266b64f2cdeb2a8dd968c40165
Malivil <mlebson@outlook.com>
2024-05-21 07:37:50 -0400

    Fixed team death notification not working for hidden roles

--- gamemodes/terrortown/gamemode/cl_deathnotify.lua
+++ gamemodes/terrortown/gamemode/cl_deathnotify.lua
@@ -14,6 +14,8 @@ AddHook("Initialize", "DeathNotify_Initialize", function()
     LANG.AddToLanguage("english", "deathnotify_ply_mid", ", they were ")
     LANG.AddToLanguage("english", "deathnotify_ply_end", "!")
     LANG.AddToLanguage("english", "deathnotify_team_mid", ", they were on the ")
+    LANG.AddToLanguage("english", "deathnotify_team_mid_hidden", ", they were on a ")
+    LANG.AddToLanguage("english", "deathnotify_team_mid_hidden_name", "hidden")
     LANG.AddToLanguage("english", "deathnotify_team_end", " team!")
     LANG.AddToLanguage("english", "deathnotify_fell", "You fell to your death!")
     LANG.AddToLanguage("english", "deathnotify_water", "You drowned!")
@@ -33,9 +35,18 @@ net.Receive("TTT_ClientDeathNotify", function()
         local col = ROLE_COLORS_HIGHLIGHT[role]
         chat.AddText(COLOR_WHITE, GetTranslation("deathnotify_ply_start"), col, name, COLOR_WHITE, GetTranslation("deathnotify_ply_mid"), col, roleString, COLOR_WHITE, GetTranslation("deathnotify_ply_end"))
     elseif reason == "team" then
-        local teamName, teamColor = GetRoleTeamInfo(role, true)
-        print(role, teamName, teamColor)
-        chat.AddText(COLOR_WHITE, GetTranslation("deathnotify_ply_start"), teamColor, name, COLOR_WHITE, GetTranslation("deathnotify_team_mid"), teamColor, teamName, COLOR_WHITE, GetTranslation("deathnotify_team_end"))
+        local midTranslation = "deathnotify_team_mid"
+        local teamName, teamColor
+        -- Special logic for if the role of the player is hidden
+        if role == ROLE_NONE then
+            midTranslation = midTranslation .. "_hidden"
+            teamName = GetTranslation("deathnotify_team_mid_hidden_name")
+            teamColor = ROLE_COLORS_HIGHLIGHT[role]
+        else
+            teamName, teamColor = GetRoleTeamInfo(role, true)
+        end
+
+        chat.AddText(COLOR_WHITE, GetTranslation("deathnotify_ply_start"), teamColor, name, COLOR_WHITE, GetTranslation(midTranslation), teamColor, teamName, COLOR_WHITE, GetTranslation("deathnotify_team_end"))
     else
         chat.AddText(COLOR_WHITE, GetTranslation("deathnotify_" .. reason))
     end
