37e211a692fc56857754f24ce6a7488943fa36b2
Logan <lobsterlogan43@yahoo.com>
2025-01-17 15:00:41 -0600

    Added PLY:IsroleAbilityDisabled

--- gamemodes/terrortown/gamemode/player_ext.lua
+++ gamemodes/terrortown/gamemode/player_ext.lua
@@ -728,6 +728,10 @@ function plymeta:ClearProperty(name, targets)
     SYNC:ClearPlayerProperty(self, name, targets)
 end
 
+function plymeta:IsRoleAbilityDisabled(...)
+    return hook.Call("TTTIsRoleAbilityDisabled", nil, self, ...) == true
+end
+
 -- Run these overrides when the round is preparing the first time to ensure their addons have been loaded
 hook.Add("TTTPrepareRound", "PostLoadOverride", function()
     -- Compatibility with Dead Ringer (810154456)
